6976bc44151e3fbd6e4451c9801c6636e76c50a1
ThreeCoffes <leonard.madzielewski@gmail.com>
2025-04-30 15:59:03 +0200

    switch to different way of managing enemy types

--- scenes/components/controlers/patrolling_ai_component.gd
+++ scenes/components/controlers/patrolling_ai_component.gd
@@ -4,13 +4,19 @@ extends Node
 @export_range(-1,1,2) var initial_direction: float = -1
 @export var left_raycast: RayCast2D
 @export var right_raycast: RayCast2D
+@export var is_patrolling: bool = true
 
 var input_horizontal: float = 0
 
 func _ready() -> void:
+	if !is_patrolling:
+		return
 	input_horizontal = initial_direction
 	
 func _process(_delta: float) -> void:
+	if !is_patrolling:
+		input_horizontal = 0
+		return
 	if !left_raycast == null and left_raycast.is_colliding():
 		input_horizontal = 1
 	elif !right_raycast == null and right_raycast.is_colliding():
@@ -19,3 +25,8 @@ func _process(_delta: float) -> void:
 func get_jump_input() -> JumpInput:
 	return JumpInput.new()
 	
+func get_direction() -> float:
+	if is_patrolling:
+		return input_horizontal
+	else:
+		return initial_direction
--- scenes/entities/enemy.gd
+++ scenes/entities/enemy.gd
@@ -1,15 +1,33 @@
+@tool
 class_name Enemy
 extends CharacterBody2D
 
 @export_category("Nodes")
 @export var gravity_component: GravityComponent
-@export var ai_component: AIComponent
+@export var movement_ai_component: Node
 @export var animation_component: AnimationComponent
+@export var movement_component: MovementComponent
 
 func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
 	gravity_component.handle_gravity(self, delta)
+	movement_component.handle_horizontal_movement(self, movement_ai_component.input_horizontal)
 
-	animation_component.handle_move_animation(1.0) # TODO: take direciton from where they're aiming
+	animation_component.handle_move_animation(movement_ai_component.get_direction())
 	animation_component.handle_jump_animation(false, !is_on_floor())
 
 	move_and_slide()
+	
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	if gravity_component == null:
+		warnings.append("Enemy needs a gravity component")
+	if movement_ai_component == null:
+		warnings.append("Enemy needs a movement ai component")
+	if animation_component == null:
+		warnings.append("Enemy needs an animation component")
+	if movement_component == null:
+		warnings.append("Enemy needs a movement component")
+	return warnings
